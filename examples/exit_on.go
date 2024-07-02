bfcc89c02a365ab5febffc6b118e5ffe55dc3bb9
Sam Caldwell <github@samcaldwell.net>
2024-07-02 09:35:38 -0500

    fix code style error

--- examples/exit_on.go
+++ examples/exit_on.go
@@ -10,7 +10,7 @@ import (
 //
 // This program will allow the user to test the On() functionality of the exit package.
 func main() {
-	show_error := flag.Bool("show_error", false, "")
+	showError := flag.Bool("show_error", false, "")
 	lhs := flag.String("lhs", "", "")
 	rhs := flag.String("rhs", "", "")
 	n := flag.Int("code", 0, "")
@@ -18,7 +18,7 @@ func main() {
 	flag.Parse()
 	code := exit.Code(*n)
 	var err error
-	if *show_error {
+	if *showError {
 		err = fmt.Errorf("lhs (%v) != rhs (%v) returns code (%v) usage: %s", *lhs, *rhs, code, *usage)
 	}
 	exit.On(*lhs == *rhs, code, err, *usage)
