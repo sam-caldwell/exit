a3839c40edd4bc63e28ed3bfcd557cfe27eac609
Sam Caldwell <github@samcaldwell.net>
2024-07-02 10:03:30 -0500

    add tests for Code type and Code.ToInt()

---
+++ Code_test.go
@@ -0,0 +1,26 @@
+package exit_test
+
+import (
+	"github.com/sam-caldwell/exit/v2"
+	"testing"
+)
+
+func TestExit_Code(t *testing.T) {
+	t.Run("Verify 8-bit value range", func(t *testing.T) {
+		var c exit.Code
+		for i := -512; i < 512; i++ {
+			c = exit.Code(i)
+			if c < 0 || c > 255 {
+				t.Fatalf("c is out of bounds")
+			}
+		}
+	})
+	t.Run("Verify Code.ToInt()", func(t *testing.T) {
+		for i := 0; i < 255; i++ {
+			c := exit.Code(i)
+			if c.ToInt() != i {
+				t.Fatalf("mismatched value")
+			}
+		}
+	})
+}
